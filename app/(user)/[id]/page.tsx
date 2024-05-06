3afe5cd6fab5b8335a8a27c19a866773c500bb08
sk <mrrkhann9u@email.com>
2024-05-06 23:17:17 +0700

    finalize modify

--- app/(user)/[id]/page.tsx
+++ app/(user)/[id]/page.tsx
@@ -5,9 +5,9 @@ import { Metadata, ResolvingMetadata } from "next";
 
 type Props = {
 	params: { id: string };
-	name: { title: string};
 	searchParams: { [key: string]: string | string[] | undefined };
 };
+
 
 const getData = async (id: string) => {
 	const res = await fetch(`${BASE_URL}/api/products/${id}`);
@@ -24,7 +24,7 @@ export async function generateMetadata(
 	const id = params.id;
 
 	// fetch data
-	const product = await fetch(`https://fakestoreapi.com/products/${id}`).then((res) => res.json());
+	const product = await fetch(`${BASE_URL}/api/products/${id}`).then((res) => res.json());
 
 	// optionally access and extend (rather than replace) parent metadata
 	// const previousImages = (await parent).openGraph?.images || [];
